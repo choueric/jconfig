b6b057b5bb1721244e4663c2dbade3ba48288ddb
choueric <zhssmail@gmail.com>
2017-01-11 16:40:21 +0800

    add doc.go

---
+++ doc.go
@@ -0,0 +1,30 @@
+// Simple Example
+//
+// package main
+//
+// import (
+// 	"fmt"
+// 	"github.com/choueric/jconfig"
+// )
+//
+// const DefContent = `{
+// 	"server": "127.0.0.1:8088"
+// }
+// `
+//
+// type Config struct {
+// 	Server string `json:"server"`
+// }
+//
+// func getConfig() *Config {
+// 	// NOTE: Config{} is the your own type to store configurations.
+// 	jc := jconfig.New("config.json", Config{})
+//
+// 	if _, err := jc.Load(DefContent); err != nil {
+// 		fmt.Println("load config error:", err)
+// 		return nil
+// 	}
+//
+// 	return jc.Data().(*Config) // convert to your own type and return.
+// }
+package jconfig
