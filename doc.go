791bff9948e154ca06434a948d753edd688e465d
zhs <haishan.zhou@magewell.com>
2017-01-15 14:09:15 +0800

    update doc.go

--- doc.go
+++ doc.go
@@ -1,30 +1,30 @@
 // Simple Example
 //
-// package main
+//  package main
 //
-// import (
-// 	"fmt"
-// 	"github.com/choueric/jconfig"
-// )
+//  import (
+//  	"fmt"
+//  	"github.com/choueric/jconfig"
+//  )
 //
-// const DefContent = `{
-// 	"server": "127.0.0.1:8088"
-// }
-// `
+//  const DefContent = `{
+//  	"server": "127.0.0.1:8088"
+//  }
+//  `
 //
-// type Config struct {
-// 	Server string `json:"server"`
-// }
+//  type Config struct {
+//  	Server string `json:"server"`
+//  }
 //
-// func getConfig() *Config {
-// 	// NOTE: Config{} is the your own type to store configurations.
-// 	jc := jconfig.New("config.json", Config{})
+//  func getConfig() *Config {
+//  	// NOTE: Config{} is the your own type to store configurations.
+//  	jc := jconfig.New("config.json", Config{})
 //
-// 	if _, err := jc.Load(DefContent); err != nil {
-// 		fmt.Println("load config error:", err)
-// 		return nil
-// 	}
+//  	if _, err := jc.Load(DefContent); err != nil {
+//  		fmt.Println("load config error:", err)
+//  		return nil
+//  	}
 //
-// 	return jc.Data().(*Config) // convert to your own type and return.
-// }
+//  	return jc.Data().(*Config) // convert to your own type and return.
+//  }
 package jconfig
